7f3bae24c590ba97db3a95348563246200f35326
huajustin <huajustinh@gmail.com>
2020-05-09 23:47:01 -0700

    Added command handler and a track command (should have split into two commits)

---
+++ commands/track.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'track',
+	description: 'Begins to track a summoner whenever they start a new game',
+	usage: 'track [summoner name]',
+    cooldown: 5,
+    summoners: [],
+	execute(message, args) {
+        if (args.length === 0) {
+            return message.channel.send("Usage: !track <Summoner Name>", { reply: message.author });
+        }
+        
+        // since summoner names may contain spaces, they may be counted as multiple arguments
+        let summonerName = args.join(' ');
+
+        console.log(`Now tracking ${summonerName}`);
+        message.reply(`Now tracking ${summonerName}. Use \'!untrack ${summonerName}\' to stop tracking`);
+        
+        // keep track of our summoners
+        this.summoners.push(summonerName);
+    },
+};
--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
+const fs = require('fs');
 
 const client = new Discord.Client();
 
+// create command handler by importing local command modules and mapping to a collection
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    let reqCommand = require(`./commands/${file}`);
+    client.commands.set(reqCommand.name, reqCommand);
+}
+
+// verify we have logged in
 client.on('ready', () => {
-    console.log(`Logged in!`);
+    console.log('Logged in!');
 });
 
+// scan new messages from users and execute commands if valid
 client.on('message', (msg) => {
     if (!msg.content.startsWith(prefix) || msg.author.bot) {
         return;
     }
 
-    let query = msg.content.slice(prefix.length).split(' ');
-    let command = query.shift().toLowerCase();
+    // separate command and arguments
+    let args = msg.content.slice(prefix.length).split(' ');
+    let command = args.shift().toLowerCase();
 
-    
+    // lookup and execute command
+    try {
+        client.commands.get(command).execute(msg, args);
+    } catch (error) {
+        console.error(error);
+        msg.reply("Sorry, I couldn't execute that command!");
+    }
 });
 
 client.login(token);
