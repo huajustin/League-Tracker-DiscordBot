94404e103ad222002435c9f8b5d92bdd0d0af526
huajustin <huajustinh@gmail.com>
2020-05-17 00:59:12 -0700

    Added skeleton untrack command, and global set of trackees

--- commands/track.js
+++ commands/track.js
@@ -1,24 +1,27 @@
 const summonerCall = require('../endpoints/riot/summoner.js');
+const summoners = require('../endpoints/riot/resources/tracked_summoners.js');
 
 module.exports = {
 	name: 'track',
 	description: 'Begins to track a summoner whenever they start a new game',
 	usage: 'track [summoner name]',
     cooldown: 5,
-    summoners: [],
 	execute(message, args) {
         if (args.length === 0) {
             return message.channel.send("Usage: !track <Summoner Name>", { reply: message.author });
         }
         
         // since summoner names may contain spaces, they may be counted as multiple arguments
-        let summonerName = args.join(' ');
+        const summonerName = args.join(' ');
 
         console.log(`Now tracking ${summonerName}`);
         message.reply(`Now tracking ${summonerName}. Use \'!untrack ${summonerName}\' to stop tracking`);
         
+        // TODO: Need to implement API monitor to watch for changes.. then add summoner
+        // to be monitored
+
         // keep track of our summoners
-        this.summoners.push(summonerName);
+        summoners.add(summonerName);
         summonerCall(summonerName);
     },
 };
---
+++ commands/untrack.js
@@ -0,0 +1,23 @@
+const summoners = require('../endpoints/riot/resources/tracked_summoners.js');
+
+module.exports = {
+    name: 'untrack',
+	description: 'Stops tracking a summoner',
+	usage: 'untrack [summoner name]',
+    cooldown: 5,
+    execute(message, args) {
+        if (args.length === 0) {
+            return message.channel.send("Usage: !untrack <Summoner Name>", { reply: message.author });
+        }
+
+        const summonerName = args.join(" ");
+
+        summoners.delete(summonerName);
+
+        // TODO: Need to implement API monitor to watch for changes.. then remove summoner
+        // from monitor
+
+        console.log(`Tracking ${summonerName} stopped`);
+        message.reply(`Tracking ${summonerName} stopped. Use \'!track ${summonerName}\' to start tracking again`);
+    }
+}
